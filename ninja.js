30d2c9aafe56fcce760240d72cd942ca04948c5a
cheojorge <cheojorge@hotmail.com>
2022-01-06 19:11:42 -0500

    [TAREA] sensei

--- ninja.js
+++ ninja.js
@@ -1,9 +1,9 @@
 class Ninja{
-	constructor(nombre,salud){
+	constructor(nombre,salud,velocidad,fuerza){
 		this.nombre = nombre ;
 		this.salud = salud || 10;
-		this.velocidad = 3;
-		this.fuerza = 3;
+		this.velocidad = velocidad || 3;
+		this.fuerza = fuerza || 3;
 	}
 	sayName(){
 		console.log(`El nombre del ninja es ${ this.nombre }`)
@@ -19,8 +19,19 @@ class Ninja{
 	}
 
 }
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake();
-ninja1.showStats();
+
+class Sensei extends Ninja{
+	constructor(nombre){
+		super(nombre,200,10,10);
+		this.sabiduria = 10;
+	}
+	speakWisdom(){
+		super.drinkSake();
+		console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.")
+	}
+}
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
